1e609f016a96be19de7fc67a952d7bede8a5c76f
DiwakarVenkatesan <diwakar.venkatesan@syncfusion.com>
2023-12-11 19:04:04 +0530

    Added sample and ReadMe.

---
+++ MauiPopup/MauiPopup/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiPopup
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.clickToShowPopup.Clicked += ClickToShowPopup_Clicked;
+        }
+
+        private void ClickToShowPopup_Clicked(object? sender, EventArgs e)
+        {
+            this.popup.Show();
+        }
+    }
+
+}
