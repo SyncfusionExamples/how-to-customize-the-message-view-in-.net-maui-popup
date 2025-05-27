11297e09f246af29c92f5cafe3d2a3b3b3c4da49
RiyashameedM <riyashameed.m@syncfusion.com>
2025-05-27 12:01:40 +0530

    Included App.xaml changes

--- MauiPopup/MauiPopup/App.xaml.cs
+++ MauiPopup/MauiPopup/App.xaml.cs
@@ -1,12 +1,16 @@
-﻿namespace MauiPopup
+﻿
+namespace MauiPopup
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
         }
-    }
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			return new Window(new AppShell());
+		}
+	}
 }
